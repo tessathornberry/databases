839889e8bf4cbd837759b1c7f7624f112f958fe0
Tessa Thornberry <tthornberryclass@gmail.com>
2022-10-11 19:05:57 -0700

    made some changes, but testing github token

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 
 // Set up our routes
-app.use('/classes', router);
+app.use('/classes', router); //why is this under classes?
 
 // Serve the client files
 app.use(express.static(__dirname + '/../client'));
